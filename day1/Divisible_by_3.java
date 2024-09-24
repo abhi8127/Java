2bad1b6fc29f6cd223f032aa90cfca21c7cb0a62
abhi8127 <abhisheky06880@gmail.com>
2024-09-24 18:11:34 +0530

    Divisible by 3

---
+++ day1/Divisible_by_3.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Divisible_by_3 {
+    public static void main(String[] args){
+        int n=6;
+        if(n%3==0)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+}
