ecded17f64a75c32fd5360921791de385d628f9d
abhi8127 <abhisheky06880@gmail.com>
2024-09-24 17:49:20 +0530

    cube of a number

---
+++ day1/Cube_of_a_number.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Cube_of_a_number {
+    public static void main(String[] args){
+        int n=5;
+        int ans=(int)Math.pow(n,3);
+        System.out.println(ans);
+    }
+}
