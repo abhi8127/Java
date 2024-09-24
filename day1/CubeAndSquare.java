27fd8aab351674c15b12374d047eac70784384cb
abhi8127 <abhisheky06880@gmail.com>
2024-09-24 18:05:48 +0530

    Square and cube

---
+++ day1/CubeAndSquare.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class CubeAndSquare {
+    public static void main(String[] args){
+        int m=3;
+        int n=2;
+        int cube =(int)Math.pow(n,3);
+        double square=Math.pow(m,2);
+        System.out.println(cube>square);
+    }
+}
