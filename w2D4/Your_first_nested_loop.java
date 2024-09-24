a23491d0b4f6a95fe559a7d1b495ba320b4c08eb
abhi8127 <abhisheky06880@gmail.com>
2024-09-24 18:22:15 +0530

    nested loop

---
+++ w2D4/Your_first_nested_loop.java
@@ -0,0 +1,16 @@
+package w2D4;
+
+public class Your_first_nested_loop {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
