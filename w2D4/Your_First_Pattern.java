a6fbaff189fb7c7cea5eda6daac77be1c9f5b0b8
abhi8127 <abhisheky06880@gmail.com>
2024-09-25 18:31:44 +0530

    first pattern

---
+++ w2D4/Your_First_Pattern.java
@@ -0,0 +1,14 @@
+package w2D4;
+
+public class Your_First_Pattern {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
