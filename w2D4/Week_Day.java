95e960552299cc4322bc566b0b8ad557509baaf5
abhi8127 <abhisheky06880@gmail.com>
2024-09-26 18:31:59 +0530

    Switch case

---
+++ w2D4/Week_Day.java
@@ -0,0 +1,32 @@
+package w2D4;
+
+public class Week_Day {
+    public static void main(String[] args){
+        int num=4;
+        switch (num){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wenesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+
+        }
+    }
+}
